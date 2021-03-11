35d413078b0c75f34f2c33cbc132e89ab7f51151
Ludmylla Arielly <ludmay4@gmail.com>
2021-03-11 15:12:23 -0300

    Correcting product endpoint

--- src/main/java/com/game/store/service/ProductServiceImpl.java
+++ src/main/java/com/game/store/service/ProductServiceImpl.java
@@ -43,7 +43,7 @@ public class ProductServiceImpl implements ProductService{
 	@Override
 	public List<Product> productOrderAlphabetical() {
 		List<Product> product = new ArrayList<Product>(productRepository.findAll());
-		product.sort(Comparator.comparing(Product::getName).reversed());
+		product.sort(Comparator.comparing(Product::getName));
 		
 		return product;
 	}
